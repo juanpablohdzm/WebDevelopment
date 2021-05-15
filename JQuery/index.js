31779088ab57a144855947691865a61e933663a0
juanp <juanpablohdzm95@gmail.com>
2021-05-15 10:23:47 -0500

    testing jquery.

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,3 +1,12 @@
 
 ///$(document).ready(function(){alert("Im ready");});
 $("h1").css("color","red");
+
+var color = $("h1").css("color"); //color is red
+
+var bt =$("h1").addClass("big-title new-color");
+bt.css("color","blue");
+//.removeClass("big-title");
+bt.css("color","green"); //You are just adding an inline style to the tag. 
+
+$("h1").hasClass("big-title");
