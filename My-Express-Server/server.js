96092ac30f3ed352f35fc78e6dce3da9562547d0
juanp <juanpablohdzm95@gmail.com>
2021-05-20 08:37:38 -0500

    Add nodemon to the project.

--- My-Express-Server/server.js
+++ My-Express-Server/server.js
@@ -1,7 +1,23 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
 
-app.listen(3000, function()
-{
-          console.log("Server started on port 3000");
+app.get("/", function (request, response) {
+  response.send("www.google.com");
 });
+
+app.get("/contact", function (req, res) {
+          res.send("Contact me at: angela@gmail.com");
+});
+
+app.get("/hobbies",function(req, res)
+{
+          res.send("No ginger");
+})
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
+
+//nodemon is a utility that will monitor for any changes in your source and will restart the server automatically
+
