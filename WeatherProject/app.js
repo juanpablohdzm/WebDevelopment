66e7475e0353e89594260af5489c624f98098ebb
juanp <juanpablohdzm95@gmail.com>
2021-05-21 10:17:19 -0500

    send multiple things.

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -12,22 +12,22 @@ app.get("/", function (req, res) {
     .get(
       "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e72ca729af228beabd5d20e3b7749713&units=metric",
       function (response) {
-
         console.log(response.statusCode);
         response.on("data", function (data) {
-
           let weatherData = JSON.parse(data);
-          console.log(weatherData); //Check the path for the information we need. 
+          console.log(weatherData); //Check the path for the information we need.
           let temp = weatherData.main.temp;
-          console.log(temp);
-
+          let imageURL = "http://openweathermap.org/img/wn/10d@2x.png";
+          res.write("The weather is currently"+ weatherData.main.temp);
+          res.write(
+            "<h1>The temperature in London is " + temp + "degrees Celcius</h1>"
+            ); 
+          res.write("<img src="+imageURL+">");
+          res.send();//You can only have one send.
         });
       }
     )
     .on("error", function (e) {
-
       console.error(e);
-
     });
-  res.send("Server is up and running");
 });
