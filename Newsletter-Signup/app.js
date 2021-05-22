1a997e7d4dea3b8f81cdc0ae715d1e079f26528e
juanp <juanpablohdzm95@gmail.com>
2021-05-22 09:13:33 -0500

    send information to a database.

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -12,11 +12,49 @@ app.listen(3000, function () {
 });
 
 app.post("/", function (req, res) {
-  let firstName = req.body.fName;
-  let lastName = req.body.lName;
-  let email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  const apiKey = "";
+  const url = `https://us4.api.mailchimp.com/3.0/lists/${apiKey}`;
+  const options = {
+    method: "POST",
+    auth: `juanp:${apiKey}`,
+  };
+
+  const request = https.request(url, options, function (response) {
+
+    if(response.statusCode === 200)
+    {
+      res.sendFile(__dirname+"/success.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.on("error",function(error){
+    res.sendFile(__dirname+"/failure.html");
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.get("/", function (req, res) {
