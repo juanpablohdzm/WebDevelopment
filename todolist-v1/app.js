a0a4add1e3d0ad12b18a90695d72b90acd8489f5
juanp <juanpablohdzm95@gmail.com>
2021-05-27 13:33:41 -0500

    Starting with ejs.

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,15 +2,42 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.listen(3000, function (req, res) {
   console.log("Server ready");
 });
 
 app.get("/", function (req, res) {
   let today = new Date();
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res.send("Yay it is the weekend!");
-  } else {
-    res.sendFile(__dirname+"/index.html");
+  let dayIndex = today.getDay();
+  let day = "";
+  switch (dayIndex) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+
+    default:
+      break;
   }
+
+  res.render("list", { kindOfDay: day });
 });
