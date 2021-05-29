001010e593668fe4687aa7e9696ec745ee15f9ce
juanp <juanpablohdzm95@gmail.com>
2021-05-29 16:30:46 -0500

    Add for loop skeleton for list

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,41 +3,31 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
 
 app.listen(3000, function (req, res) {
   console.log("Server ready");
 });
 
+var items=[];
+
 app.get("/", function (req, res) {
+
   let today = new Date();
-  let dayIndex = today.getDay();
-  let day = "";
-  switch (dayIndex) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
 
-    default:
-      break;
-  }
+  let options = {
+    weekday : 'long',
+    day : "numeric",
+    month : "long"
+  };
 
-  res.render("list", { kindOfDay: day });
+  let day  = today.toLocaleDateString("en-US",options);
+
+  res.render("list", { kindOfDay: day,items: items });
+});
+
+app.post("/", function(req, res){
+  item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
