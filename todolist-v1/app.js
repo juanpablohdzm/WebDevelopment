f290d60af089bb94edfe27a9e060ec766eddc224
juanp <juanpablohdzm95@gmail.com>
2021-06-01 13:16:50 -0500

    add ejs layouts.

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -12,14 +12,36 @@ app.listen(3000, function (req, res) {
 });
 
 var items=[];
+var workItems = [];
 
 app.get("/", function (req, res) {
   let day = date.getDate();
-  res.render("list", { kindOfDay: day,items: items });
+  res.render("list", { listTitle: day,items: items });
 });
 
 app.post("/", function(req, res){
-  item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+
+  let item = req.body.newItem;
+  if(req.body.list === "Work")
+  {
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else
+  {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+
+app.get("/work",function(req,res){
+    res.render("list",{listTitle:"Work List", workItems});
+});
+
+app.post("/work", function(req,res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
