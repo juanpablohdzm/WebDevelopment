5b616d4fccb8dd3ac5e82c06d58b950a82f62920
juanp <juanpablohdzm95@gmail.com>
2021-06-28 09:43:26 -0500

    Add encryption package for mongoose.

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -13,10 +14,13 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useUnifiedTopology: true,
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+const secret = "ThisIsOurLittleSecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
