416ba4454e8ecbe3a65afce5db89688d05992620
juanp <juanpablohdzm95@gmail.com>
2021-08-18 11:05:59 -0500

    Mappind data to components and render a list of notes.

--- React-Module/keeper-app/src/components/App.jsx
+++ React-Module/keeper-app/src/components/App.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Note/>
-      <Footer/>
+      <Header />
+      {notes.map((note) => {
+        return (
+          <Note key={note.key} title={note.title} content={note.content} />
+        );
+      })}
+      <Footer />
     </div>
   );
 }
